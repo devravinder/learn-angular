bb29520c77b64ac95dd5f370779c2eb78a31b15b
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-02-04 09:34:02 +0530

    fix: typo

--- v21/todo-app/src/app/app.routes.ts
+++ v21/todo-app/src/app/app.routes.ts
@@ -9,13 +9,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/settings/settings'),
   },
 
-
-
   // order is important, bcz /anything will be treated as :id, so keep it last
   {
     path: '',
     // pathMatch: 'full',// it creates a conflict with child routes because the entire URL must be empty to match.
-    title: 'Taks',
+    title: 'Tasks',
     // we can load diffrent component based on user
     // we can inject anything in loadComponent method & implement logic
     loadComponent: () => import('./pages/dashboard/dashboard'),
@@ -25,12 +23,11 @@ export const routes: Routes = [
         resolve: { data: archiveTasksResolver },
         loadComponent: () => import('./pages/dashboard/archive/archive'),
       },
-       {
+      {
         path: ':id',
         resolve: { task: taskResolver },
         loadComponent: () => import('./pages/dashboard/task-details/task-details'),
       },
     ],
   },
-  
 ];
